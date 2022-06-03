62d9dd943ccc191dbed5bb1e54e25af816d140af
CodeConscious <hello@codeconscio.us>
2022-06-03 21:58:11 +0900

    Improve print method

--- FunctionalCSharpDojo/Program.cs
+++ FunctionalCSharpDojo/Program.cs
@@ -4,6 +4,19 @@ using System.Runtime.CompilerServices;
 using static LanguageExt.Prelude;
 using static System.Console;
 
+static int EvalWith5ThenAdd2(Func<int, int> fn) => fn(5) + 2;
+var square = (int x) => x * x;
+PrintWithExpression(EvalWith5ThenAdd2(square));
+
+static Func<int, int> MultipierGenerator(int toMultiply) => x => x * toMultiply;
+var multiplyBy3 = MultipierGenerator(3);
+PrintWithExpression(multiplyBy3(2));
+var multiplyBy200 = MultipierGenerator(200);
+PrintWithExpression(multiplyBy200(2));
+var multiplyBy100000 = MultipierGenerator(multiplyBy200(500));
+PrintWithExpression(multiplyBy100000(7));
+
+
 static void PrintWithExpression(object result, [CallerArgumentExpression("result")] string? expression = null)
 {
     if (!string.IsNullOrWhiteSpace(expression))
@@ -13,15 +26,13 @@ static void PrintWithExpression(object result, [CallerArgumentExpression("result
         ResetColor();
     }
 
-    WriteLine("      " + result);
+    string output = result switch
+    {
+        int i => i.ToString("#,##0"),
+        double d => d.ToString("#,##0.#"),
+        decimal d => d.ToString("#,##0.#"),
+        _ => result?.ToString() ?? "<NULL>"
+    };
+
+    WriteLine("      " + output);
 }
-
-static int EvalWith5ThenAdd2(Func<int, int> fn) => fn(5) + 2;
-var square = (int x) => x * x;
-PrintWithExpression(EvalWith5ThenAdd2(square));
-
-static Func<int, int> MultipierGenerator(int toMultiply) => x => x * toMultiply;
-var multipierBy3 = MultipierGenerator(3);
-var multipierBy200 = MultipierGenerator(200);
-PrintWithExpression(multipierBy3(2));
-PrintWithExpression(multipierBy200(2));
